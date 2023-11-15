43b44444394f44f92749af77f603e9362ca96684
Nicholas Kolodziej <Nicholas.Kolodziej@ibm.com>
2023-11-15 12:39:29 -0600

    updated aesthetics - removed extras

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { SplashComponent } from './screens/splash/splash.component';
-import { ProjectsComponent } from './screens/projects/projects.component';
-import { AboutComponent } from './screens/about/about.component';
-import { ContactComponent } from './screens/contact/contact.component';
-
 const routes: Routes = [
-  {
-    path: "",
-    component: SplashComponent
-  },
-  {
-    path: "projects",
-    component: ProjectsComponent
-  },
-  {
-    path: "about",
-    component: AboutComponent
-  },
-  {
-    path: "contact",
-    component: ContactComponent
-  },
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,28 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SplashComponent } from './screens/splash/splash.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { ProjectsComponent } from './screens/projects/projects.component';
-import { AboutComponent } from './screens/about/about.component';
-import { ContactComponent } from './screens/contact/contact.component';
+import { MatCardModule } from '@angular/material/card';
+
 import { HeaderComponent } from './components/header/header.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SplashComponent,
-    ProjectsComponent,
-    AboutComponent,
-    ContactComponent,
     HeaderComponent,
-    NavigationComponent
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
@@ -35,7 +29,8 @@ import { NavigationComponent } from './components/navigation/navigation.componen
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
